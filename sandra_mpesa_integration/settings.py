ca72d7ac130e3e14389d2b064549b4dbdba8447f
eMobilis <sandrasasimwa@gmail.com>
2023-03-09 10:38:41 +0300

    mpesa

--- sandra_mpesa_integration/settings.py
+++ sandra_mpesa_integration/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sandra_mpesa_integration',
+    'django_daraja'
 ]
 
 MIDDLEWARE = [
